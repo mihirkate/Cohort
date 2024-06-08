8b8f95af415de750eb6a294e5e30c66f8a419111
mihirkate <mihirkate13@gmail.com>
2024-06-08 19:40:06 +0530

    added backend

--- week-8/Paytm-2/backend/routes/account.js
+++ week-8/Paytm-2/backend/routes/account.js
@@ -4,23 +4,33 @@ const { default: mongoose } = require("mongoose");
 const { authMiddleware } = require("../middleware");
 const router = express.Router();
 
-router.get("/balance", authMiddleware, async (req, res) => {
-    const account = await Account.findOne({
-        userId: req.userId,
-    });
-    console.log(account);
-    res.json({
-        balance: account.balance,
-    });
+router.get('/balance', authMiddleware, async (req, res) => {
+
+    try {
+        const account = await Account.findOne({
+            userId: req.userId
+        });
+        console.log(req.userId)
+        console.log(account);
+        if (!account) {
+            return res.status(404).json({ message: 'Account not found' });
+        }
+        res.json({
+            balance: account.balance,
+        });
+
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 
 router.post("/transfer", authMiddleware, async (req, res) => {
     const session = await mongoose.startSession();
     session.startTransaction();
-    console.log(session);
-    const { amount, to } = req.body;
 
+    const { amount, to } = req.body;
+    console.log(to);
     if (!amount || amount.balance < amount) {
         session.abortTransaction();
 
@@ -29,22 +39,23 @@ router.post("/transfer", authMiddleware, async (req, res) => {
         });
     }
     const toAccount = await Account.findOne({ userId: to }).session(session);
-
+    console.log("to account :", toAccount);
     if (!toAccount) {
         session.abortTransaction();
 
         return res.status(400).json({
-            message: "Invalid account "
+            message: "Invalid account"
         });
     }
 
-    Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
-    Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
+    await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
+    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
+
 
 
     // commit the transactiob
     await session.commitTransaction();
-
+    console.log(toAccount.balance);
     res.json({
         message: "Transfer successful"
     });
--- week-8/Paytm-2/frontend/src/App.jsx
+++ week-8/Paytm-2/frontend/src/App.jsx
@@ -1,11 +1,19 @@
-
+import { BrowseRouter, Route } from "react-router-dom";
+import Signup from "./Pages/Signup";
+import Signin from "./Pages/Signup";
+import Dashboard from "./Pages/Dashboard";
+import SendMoney from "./Pages/SendMoney";
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <BrowseRouter>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </BrowseRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
