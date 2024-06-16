056ba78bb0f7d59bfb90bc0d366baf06cb85719d
mihirkate <mihirkate13@gmail.com>
2024-06-16 07:39:57 +0530

    add class-1

--- week-9/class-1/src/App.jsx
+++ week-9/class-1/src/App.jsx
@@ -23,13 +23,32 @@ function useTodos(n) {
   }, [n]);
   return { todos, loading };
 }
-function App() {
-  const { todos, loading } = useTodos(5);
-  return (
-    <>{loading ? "loading..." : todos.map((todo) => <Track todo={todo} />)}</>
-  );
-}
+// function App() {
+//   // const { todos, loading } = useTodos(5);
+//   // return (
+//   //   <>{loading ? "loading..." : todos.map((todo) => <Track todo={todo} />)}</>
+//   // );
 
+//   const isOnline = useIsOnline();
+//   if (isOnline) {
+//     return "user is Online";
+//   }
+//   return "offline ";
+// }
+
+function useIsOnline() {
+  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
+  useEffect(() => {
+    window.addEventListener("online", () => {
+      setIsOnline(true);
+    });
+
+    window.addEventListener("offline", () => {
+      setIsOnline(false);
+    });
+  }, [isOnline]);
+  return isOnline;
+}
 function Track({ todo }) {
   return (
     <div>
@@ -40,4 +59,53 @@ function Track({ todo }) {
   );
 }
 
-export default App;
+const useMousePointer = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return position;
+};
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => clearTimeout(timerId);
+  }, [value, delay]);
+  return debouncedValue;
+};
+
+const SearchBar = () => {
+  const [inputValue, setInputValue] = useState("");
+  const debouncedValue = useDebounce(inputValue, 500); // 500 milliseconds debounce delay
+
+  // Use the debouncedValue in your component logic, e.g., trigger a search API call via a useEffect
+  useEffect(() => {
+    if (debouncedValue) {
+      // Example API call or any action based on debounced value
+      console.log(`API call with query: ${debouncedValue}`);
+      // fetchAPI(debouncedValue);
+    }
+  }, [debouncedValue]);
+  return (
+    <input
+      type="text"
+      value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)}
+      placeholder="Search..."
+    />
+  );
+};
+
+export default SearchBar;
