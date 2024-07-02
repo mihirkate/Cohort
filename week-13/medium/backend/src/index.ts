fe239fdb2faf9479e54ff4d3e338f8084204fb6e
mihirkate <mihirkate13@gmail.com>
2024-07-02 17:36:47 +0530

    added prisma/signin route

---
+++ week-13/medium/backend/src/index.ts
@@ -0,0 +1,48 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get("/api/v1/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    }
+  })
+
+  return c.text('signup route')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('signin route')
+})
+app.get('/api/v1/blog/:id', (c) => {
+  const id = c.req.param('id')
+  console.log(id);
+  return c.text('get blog route')
+})
+
+app.post('/api/v1/blog', (c) => {
+
+  return c.text('blog  route')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('signin route')
+})
+export default app
