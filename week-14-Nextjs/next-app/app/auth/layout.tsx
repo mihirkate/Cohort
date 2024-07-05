e9bf90b0ebb3bb93341aba96fcbce31fd6b83468
mihirkate <mihirkate13@gmail.com>
2024-07-05 16:58:52 +0530

    into to next js

---
+++ week-14-Nextjs/next-app/app/auth/layout.tsx
@@ -0,0 +1,7 @@
+export default function ({children}:{children:React.ReactNode}){
+    
+    return <div className="text-center border-b-4"  >
+        <h1> 20% of every where </h1>
+        {children}
+    </div>
+}
---
+++ week-14-Nextjs/next-app/app/auth/signin/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import  SigninComponent from '@/components/SigninComponent';
+
+export default function Signin() {
+    return (
+        <div>
+            <SigninComponent />
+        </div>
+    );
+}
