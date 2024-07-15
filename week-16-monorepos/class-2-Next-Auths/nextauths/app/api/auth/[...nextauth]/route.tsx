46dacfa410dc0a87c7f8c772df5ba9001b33f3fa
mihirkate <mihirkate13@gmail.com>
2024-07-15 20:50:41 +0530

    added providers

--- week-16-monorepos/class-2-Next-Auths/nextauths/app/api/auth/[...nextauth]/route.tsx
+++ week-16-monorepos/class-2-Next-Auths/nextauths/app/api/auth/[...nextauth]/route.tsx
@@ -1,23 +1,8 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from 'next-auth/providers/credentials';
+import { NEXT_AUTH } from "../../lib/auth";
 
 
-const handler = NextAuth({
-    providers: [
-        CredentialsProvider({
-            name: "Email",
-            credentials: {
-                username: { label: 'Username', type: 'text' },
-                password: { label: 'Password', type: 'password' }
-            },
-            async authorize(credentials, req) {
-                return {
-                    id: "user1"
-                };
-            }
-        })
-    ],
-    secret:process.env.NEXTAUTH_SECRET
-});
+const handler = NextAuth(NEXT_AUTH);
 
 export { handler as GET, handler as POST }
--- week-16-monorepos/class-2-Next-Auths/nextauths/components/Appbar.tsx
+++ week-16-monorepos/class-2-Next-Auths/nextauths/components/Appbar.tsx
@@ -1,13 +1,18 @@
 
 "use client"
 import { useRouter } from "next/navigation"
-import { signIn, signOut } from "next-auth/react"
+import { signIn, signOut,useSession } from "next-auth/react"
 export const Appbar=()=>{
 const router=useRouter();
+const session=useSession();
     return <div>
 
         <button onClick={()=>signIn()}>Sign In </button>
         <hr />
          <button onClick={() => signOut()}>signOut</button>
+
+         {
+            JSON.stringify(session)
+         }
     </div>
 }
