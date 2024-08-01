1c18f11ac3d4907fc271055c7752b00a2d13649d
mihirkate <mihirkate13@gmail.com>
2024-08-01 14:12:56 +0530

    added middlewares

--- week-17-Paytm-monorepo/paytm-monorepo/apps/user-app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-  // import { PrismaClient } from "@repo/db/client"  
-import { PrismaClient } from '@prisma/client';
-const client=new  PrismaClient();
-export default function Page():JSX.Element{
-return <div className="text-2xl">
-  hii there 
-</div>
-}
